3445475857d063e2eaca725b13b310e75f363ae4
favorhau <1057072764@qq.com>
2021-10-28 19:08:03 +0800

    add

--- index.py
+++ index.py
@@ -1,11 +1,64 @@
+# index.py
+# version: 1.0
+
+import re
+import jwt
 from utils import Account
-from flask import Flask
+from flask import Flask, request
+import redis
 
 app = Flask(__name__)
+r = redis.Redis(host='localhost', port=6379)
+accounts = {}
 
-@app.route('/')
-def index():
-    account = Account('2006100062', '257314')
-    account.login()
-    return account.get_stu_info()
+@app.before_request
+def verify():
+    """
+    Verify the token of API use
+    """
+    _token = request.headers.get('token')
+    res = {
+    "msg": None,
+    "data": []
+    }
+    if not _token and 'verify' not in request.base_url:
+        res["msg"] = "Token needed"
+    else:
+        res_token = r.get(_token)
+        if res_token:
+            pass
+        else:
+            res["msg"] = "Token invail"
+            
+    return res
+        
+@app.route("/v1/auth", methods=["POST"])
+def auth():
+    """
+    Get the auth Token for API user to verify
+    """
+    _token = request.headers.get('token')
+    _data = eval(request.data)
+    res = {
+    "msg": None,
+    "data": {}
+    }
+    try:
+        if "username" in _data and "password" in _data:
+            pass
+        _username = _data["username"]
+        _password = _data["password"]
+        account = Account(_username, _password)
+        if account.login():
+            res["msg"] = "success"
+            res["token"] = 
+            accounts[_username] = account
+        else:
+            res["msg"] = "username or password is wrong"
+            
+    except Exception:
+        res["msg"] = "Unkown exception"
     
+    return res
+
+app.run()
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from re import A
 import redis
-
+import time
 r = redis.Redis(host='localhost', port=6379)
-a= r.set('python', '1')
-print(a)
+a= r.set('python', '1' , ex=1)
+print(r.get('python'))
+time.sleep(1)
+print(r.get('python'))
