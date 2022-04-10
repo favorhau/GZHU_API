8f131be7c5ada487fd31a295586a4738381cb8a1
situ2001 <yongcong2001@outlook.com>
2022-04-10 17:27:43 +0800

    use python ver des, remove unused fn `rsa_dec`

--- utils/rsa.py
+++ utils/rsa.py
@@ -1,14 +1,6 @@
-import execjs
+from des import strenc
 
-with open('./utils/rsa.js', 'r') as f:
-  js_code = f.read()
 
-js_complied = execjs.compile(js_code)
-
-def rsa_enc(content): 
-  js_func = js_complied.call("strEnc", content, "1", "2", "3")
-  return js_func
-  
-def rsa_dec(content):
-  js_func = js_complied.call("strDec", content, "1", "2", "3")
-  return js_func
+def rsa_enc(content):
+    result = strenc(content, "1", "2", "3")
+    return result
